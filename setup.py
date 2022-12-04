5e174223a9bced3887350552c769dad5a39d130e
jjwgs <1257400056@qq.com>
2022-12-04 20:55:37 +0800

    t

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup
 mod_name = "deepnoteHelp"
 setup(name=mod_name,
   version="1.0.1",
-  packages=["deepnoteHelp"] ,
+  packages=["DeepnoteHelp"] ,
+  package_data= {'DeepnoteHelp':"*.*"}
 )
 
 
