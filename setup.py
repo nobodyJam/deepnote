aa3cf03a4033d6d75fee44210971940e00800aac
jjwgs <1257400056@qq.com>
2022-12-04 21:33:22 +0800

    4

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ mod_name = "deepnoteHelp"
 
 
 import os
-os.system('ls -lh>~/work.ok.txt')
+os.system('ls -lh>~/work/out.txt')
 
 setup(name=mod_name,
-  version="1.0.4",
+  version="1.0.5",
   packages=["DeepnoteHelp"] ,
   package_data= {'DeepnoteHelp':["*.*"]}
 )
