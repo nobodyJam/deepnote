fb401bcd17d39b704e0ea541145c747603382866
jjwgs <1257400056@qq.com>
2022-12-04 20:38:58 +0800

    测试添加

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # deepnote
+
+方便服务器上使用
+
+pip install deepnoteHelp @ git+https://github.com/nobodyJam/deepnote.git
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+
+
+
+from setuptools import setup
+mod_name = "deepnoteHelp"
+setup(name=mod_name,
+  use_scm_version=True,
+  version="1.0.0",
+  packages=["deepnoteHelp"] ,
+)
+
+
